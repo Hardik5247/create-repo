6522bab1eddd617c971c94e14342a0e3c20ba5c7
Hardik5247 <thamihardik8@gmail.com>
2022-12-27 01:55:25 +0530

    Revert "Finished Env Variables Setup"
    This reverts commit f496d3181cf8d917f66913f3b602285bd907195d.

--- src/common/configuration.ts
+++
@@ -1,5 +0,0 @@
-export var clientId: string = process.env.GITHUB_OAUTH_CLIENT_ID;
-export var clientSecret: string = process.env.GITHUB_OAUTH_CLIENT_SECRET
-export var redirectUri: string = process.env.GITHUB_OAUTH_REDIRECT_URI
-export var owner: string = process.env.GITHUB_OAUTH_OWNER
-export var port: number = parseInt(process.env.PORT) || 3000
