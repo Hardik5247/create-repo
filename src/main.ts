f496d3181cf8d917f66913f3b602285bd907195d
Hardik5247 <thamihardik8@gmail.com>
2022-12-27 01:52:36 +0530

    Finished Env Variables Setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+require('dotenv').config()
+
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { port } from './common/configuration'
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setBaseViewsDir(join(__dirname, '..', 'assets'));
+  app.setViewEngine('hbs');
+  await app.listen(port);
 }
 bootstrap();
